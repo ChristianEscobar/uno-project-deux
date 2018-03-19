a85a0dd1debf9b1259146cfba83c5de417863bcd
Christian Escobar <crs.escobar@gmail.com>
2018-03-19 14:28:29 -0700

    latest changes

--- app/public/assets/js/game.js
+++ app/public/assets/js/game.js
@@ -2,6 +2,8 @@ const playerHandURL = "/game/hand/" + sessionStorage.playerId;
 
 const deckURL = "/game/deck";
 
+const discardURL = "/game/discard/topcard";
+
 const startGameURL = "/game/start";
 
 
@@ -24,7 +26,8 @@ $(document).ready(function(event) {
 function isGameReady() {
 	return $.get(deckURL)
 	.then((deck) => {
-		if(deck.length > 0) {
+		// Hard coding initial number of cards expected to be in the deck at the start of a game.
+		if(deck.length > 93) {
 			console.log("Game is ready.");
 
 			setupBoard();
@@ -36,9 +39,33 @@ function isGameReady() {
 }
 
 function setupBoard() {
-	$.get(deckURL)
+	refreshDeck()
+	.then((results) => {
+		refreshDiscard();
+	})
+	.then((results) => {
+		console.log("setupBoard() done");
+	})
+	.catch((error) => console.error((error)));
+}
+
+function refreshDeck() {
+	return $.get(deckURL)
 	.then((deck) => {
 		const gameDeck = new DeckPlacement("draw_pile", {object: "draw", data: deck});
+
+		return Promise.resolve(true);
+	})
+	.catch((error) => console.error(error));
+}
+
+function refreshDiscard() {
+	$.get(discardURL)
+	.then((discard) => {
+		console.log("====> Discard", discard.card.card);
+		const gameDiscard = new DiscardPlacement("discard_pile", {object: "discard", data: discard.card.card});
+
+		return Promise.resolve(true);
 	})
 	.catch((error) => console.error(error));
 }
